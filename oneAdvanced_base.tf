99dea8dcccbd7c4f408a59a4819e96762f443462
Michael Wiley <michael.wiley@oneadvanced.com>
2024-09-21 14:19:29 +0100

    update

--- oneAdvanced_base.tf
+++ oneAdvanced_base.tf
@@ -1,19 +1,58 @@
 /terraform
-  /modules
-    /private_cloud
-      main.tf
-      variables.tf
-      outputs.tf
-    /azure
-      main.tf
-      variables.tf
-      outputs.tf
-   /azure
-      main.tf
-      variables.tf
-      outputs.tf
-  /azure
-      main.tf
-      variables.tf
-      outputs.tf
+    /modules
+        /identity_service
+            main.tf
+            variables.tf
+            outputs.tf
+
+        /storage_service
+            main.tf
+            variables.tf
+            outputs.tf
+
+        /eventbus_service
+            main.tf
+            variables.tf
+            outputs.tf
+
+        /observability_service
+            main.tf
+            variables.tf
+            outputs.tf
+
+        /API_service
+            main.tf
+            variables.tf
+            outputs.tf
+
+        /AI_service
+            main.tf
+            variables.tf
+            outputs.tf
+
+        /private_cloud
+            main.tf
+            variables.tf
+            outputs.tf
+
+        /azure_compute
+            main.tf
+            variables.tf
+            outputs.tf
+
+        /awsCompute
+            main.tf
+            variables.tf
+            outputs.tf
+
+        /gcpCompute
+            main.tf
+            variables.tf
+            outputs.tf
+
+        /legacy_customer
+            main.tf
+            variables.tf
+            outputs.tf
+
 main.tf
